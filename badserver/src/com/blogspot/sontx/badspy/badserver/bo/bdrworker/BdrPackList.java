9672f8fd9a3ab0e55c7eaaf864105e74ca617ad9
xuan son tran <xuanson33bk@gmail.com>
2016-04-30 18:04:41 +0700

    + define install script template file for another pack except for badspy update pack

---
+++ badserver/src/com/blogspot/sontx/badspy/badserver/bo/bdrworker/BdrPackList.java
@@ -0,0 +1,8 @@
+package com.blogspot.sontx.badspy.badserver.bo.bdrworker;
+
+/**
+ * Copyright 2016 by sontx
+ * Created by sontx on 30/4/2016.
+ */
+public class BdrPackList {
+}
---
+++ badspy/install-another-pack.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+set dirname=testpack
+set exename=test.exe
+set dirpath=%temp%\%dirname%
+if not exist %dirpath% mkdir %dirpath%
+copy /y "%~dp0\*.*" %dirpath%
+if exist "%dirpath%\%exename%" start "" "%dirpath%\%exename%"
+endlocal
+start "" /b rd /s /q %~dp0
