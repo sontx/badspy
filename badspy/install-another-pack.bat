64a095931603ea1fb1e9e6214ad3743c44926d44
xuan son tran <xuanson33bk@gmail.com>
2016-05-01 17:22:03 +0700

    - restructure install script for another packs

--- badspy/install-another-pack.bat
+++ badspy/install-another-pack.bat
@@ -1,10 +1,11 @@
 @echo off
 setlocal
+set installdir=%temp%
 set dirname=testpack
 set exename=test.exe
-set dirpath=%temp%\%dirname%
+if "%installdir:~-1%"=="\" (set dirpath=%installdir%%dirname%) else (set dirpath=%installdir%\%dirname%)
 if not exist "%dirpath%" mkdir "%dirpath%"
-copy /y "%~dp0\*.*" "%dirpath%"
+set curdir=%~dp0
+if "%curdir:~-1%"=="\" (copy /y "%~dp0*.*" "%dirpath%") else (copy /y "%~dp0\*.*" "%dirpath%")
 if exist "%dirpath%\%exename%" start "" "%dirpath%\%exename%"
-endlocal
-start "" /b rd /s /q "%~dp0"
+start "" /b cmd /c rd /s /q "%~dp0" & start "" /b cmd /c del /f "%dirpath%\install.bat" & endlocal & exit /b
